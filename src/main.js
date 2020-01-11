eab99737443aa4c90877b47992f62e4dfcd73a5b
LaoQu <1157821899@qq.com>
2020-01-11 18:04:00 +0800

    约定路由规则完成和实现路由配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
