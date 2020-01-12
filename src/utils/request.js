7822fd621e642e4a2f33599e87f7bf0f2d383a6c
LaoQu <1157821899@qq.com>
2020-01-12 19:44:24 +0800

    处理了token失效问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@ import axios from 'axios' // 引入axios插件
 import JSONBig from 'json-bigint' // 处理大数组插件
 import store from '@/store' // 引入vuex中的store实例
 import { Store } from 'vuex'
+import router from '@/router'
 
 // 创建一个axios实例，和原来的axios没关系
 const instance = axios.create({
@@ -37,11 +38,51 @@ instance.interceptors.response.user(function (response) {
   try {
     // 将数据解构
     return response.data.data
-  } catch {
+  } catch (error) {
     return response.data
   }
-}, function (error) {
-  // 错误的时候 token容易失效
+}, async function (error) {
+  // 错误的时候 token容易失效 处理token失效的问题
+  // 如何判断失效
+  // error=>config(当前请求的配置) request(请求)response(响应)
+  if (error.response && error.response.status === 401) {
+    let toPath = { path: '/login',
+      query: {
+        redirectUrl: router.currentRoute.path } } // 跳转对象
+
+    // 表示token过期，先判断  是否有refresh_token
+    if (store.state.user.refresh_token) {
+      try {
+        // 应该发送一个请求，换取新的token
+        // 这里不应该再用instance 因为instance会再次进入拦截器  用默认的axios
+        let result = await axios({
+          method: 'put',
+          url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+          headers: {
+            Authorization: `Bearer ${store.state.user.refresh_token}`
+          }
+        })
+        store.commit('updateUser', {
+          user: {
+            token: result.data.data.token, // 拿到新的token之后
+            refresh_token: store.state.user.refresh_token // 将之前refresh_token  14天有效
+          }
+        })// 更新vuex的数据 也更新了本地缓存数据
+        return instance(error.config) // 把刚才错误的请求再次发送出去  然后将promise返回
+      } catch (error) {
+        // 如果错误  表示补救措施也没用了，应该跳转到登录页，并且  把废掉的user全都干掉
+        store.commit('clearUser') // 所有用户信息清空
+        router.push(toPath) // 跳转到回登录页
+      }
+    } else {
+      // 连refresh_token 都没有
+      // 当访问页面时=>去登录=>登录成功之后=>回到之前的页面 记住当前的地址=>登录页面=>读取地址=>跳到地址
+      // params  动态路由 user/:id
+      // query传参  user?id=123
+      // 获取当前页面地址
+      router.push(toPath)
+    }
+  }
   return Promise.reject(error)
 })
 export default instance // 导出request工具
