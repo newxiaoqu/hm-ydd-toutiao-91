edf55c6a1f327f817a4b5b940c30016d16dad94a
LaoQu <1157821899@qq.com>
2020-01-12 20:42:40 +0800

    登录基础布局，修改主题颜色完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vant from 'vant'
-import 'vant/lib/index.css'
+import 'vant/lib/index.less'
 import App from './App.vue'
 import router from './router'
 import store from './store'
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <van-cell-group>
+      <van-field label="手机号：" placeholder="请输入手机号"></van-field>
+      <van-field label="验证码：" placeholder="请输入验证码">
+        <!-- slot指定给哪个坑填内容 -->
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="btn_box">
+      <van-button type="info" size="small" round block>登 录</van-button>
+    </div>
   </div>
 </template>
 
@@ -11,4 +21,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.btn_box{
+  padding: 20px;
+}
 </style>
