143f853e06bcd4562d899c72f968c89ba1cf0948
LaoQu <1157821899@qq.com>
2020-01-18 16:16:43 +0800

    渲染我的频道

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -7,10 +7,13 @@
         <van-button v-if="!editing" @click="editing=true" size="mini" type="info" plain>编辑</van-button>
         <van-button v-else @click="editing=false" size="mini" type="danger" plain>完成</van-button>
       </div>
+      <!-- 可选频道 -->
       <van-grid class="van-hairline--left">
-        <van-grid-item v-for="index in 8" :key="index">
-          <span class="f12">频道{{index}}</span>
-          <van-icon class="btn" name="cross"></van-icon>
+        <van-grid-item v-for="(item,i) in channels" :key="item.id ">
+          <span class="f12">{{ item.name }}</span>
+          <template v-if="i!==0">
+              <van-icon v-show="editing" class="btn" name="cross"></van-icon>
+          </template>
         </van-grid-item>
       </van-grid>
     </div>
@@ -31,6 +34,14 @@ export default {
   data () {
     return {
       editing: false // 是否正在编辑
+    }
+  },
+  // 子组件接受props数据
+  props: {
+    channels: {
+      required: true,
+      type: Array,
+      default: () => [] // eslint 要求我们必须用一个函数来声明数组类型，所以用箭头函数
     }
   }
 }
